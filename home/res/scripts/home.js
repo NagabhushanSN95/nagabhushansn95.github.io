3664d9e04a97727a95344761e3892e2e0047217e
Nagabhushan S N <nagabhushansn@iisc.ac.in>
2020-11-01 21:49:57 +0530

    Files restructured.

---
+++ home/res/scripts/home.js
@@ -0,0 +1,7 @@
+// Shree KRISHNAya Namaha
+// Functions for home page
+
+function displayEmail() {
+    var email_button = document.getElementById("email");
+    email_button.innerHTML = "nagabhushans@iisc.ac.in";
+}
